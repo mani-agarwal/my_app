e673d263c36b79a146bb6bd96510610124c3f9a0
Mani <manionmission@gmail.com>
2022-09-05 00:06:06 -0700

    Chnages MAde
    Changes Made

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg bg-${props.mode}`}>
   <div className="container-fluid">
-    <a className={`navbar-brand text-${props.mode==='light'?'dark':'light'}`} href="#">{props.title}</a>
+    <a className={`navbar-brand text-${props.mode==='light'?'dark':'light'}`} href="/">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a  className={`nav-link active text-${props.mode==='light'?'dark':'light'}`} aria-current="page" href="#">Home</a>
+          <a  className={`nav-link active text-${props.mode==='light'?'dark':'light'}`} aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a className={`nav-link text-${props.mode==='light'?'dark':'light'}`} href="#">{props.about}</a>
+          <a className={`nav-link text-${props.mode==='light'?'dark':'light'}`} href="/">{props.about}</a>
         </li>
         
         
